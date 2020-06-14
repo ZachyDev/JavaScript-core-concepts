34bf4ab7ef3f71b22df2ba8aff1e78693004f6e4
ZachyDev <mosetizachary001@gmail.com>
2020-06-15 00:41:15 +0300

    IIFE Explained

--- js/IIFE/main.js
+++ js/IIFE/main.js
@@ -2,4 +2,11 @@
 
 (function(){
     console.log('This is an IIFE expression');
-}())
+})();
+
+let app = (function(){
+    let card = 123;
+    console.log('in function');
+    return {};
+})();
+console.log(app)
